b2135139b4ca957f0b8a0cc980b48f229c0df5a1
lssy2k1 <lssy2k1@gmail.com>
2023-05-23 17:41:31 +0900

    0523

---
+++ src/test/java/com/kbstar/ncp/CFRCelebrityTest.java
@@ -0,0 +1,27 @@
+package com.kbstar.ncp;
+
+import com.kbstar.util.CFRCelebrityUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Slf4j
+@SpringBootTest
+class CFRCelebrityTest {
+
+    @Value("${uploadimgdir}")
+    String imgpath;
+    @Autowired
+    CFRCelebrityUtil cfrCelebrityUtil;
+    @Test
+    void contextLoads() throws ParseException {
+        String imgname = "jun.png";
+        JSONObject result = (JSONObject) cfrCelebrityUtil.getResult(imgpath,imgname);
+        log.info(result.toJSONString());
+    }
+}
